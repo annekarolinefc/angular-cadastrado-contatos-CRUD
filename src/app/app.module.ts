c264365dd9db3f94c242a20aa7c0ca402896be1b
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-27 09:33:17 -0300

    Importação dos Módulos da Biblioteca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,25 @@ import { HttpClientModule } from '@angular/common/http'
 //IMPORTAÇÃO DO MÓDULO DE FORMULÁRIOS
 import { ReactiveFormsModule } from '@angular/forms';
 
+//IMPORTANDO OS MÓDULOS DA BIBLIOTECA
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ContactUpdateComponent } from './contact-update/contact-update.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactCreateComponent,
+    ContactUpdateComponent
   ],
 
   imports: [
@@ -22,7 +37,13 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
