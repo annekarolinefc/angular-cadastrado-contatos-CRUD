5b809d152bea2dcde7aba2a9d570b252ca6fd784
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-27 11:05:02 -0300

    Criando um novo contato

--- src/app/contact-create/contact-create.component.ts
+++ src/app/contact-create/contact-create.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { ContactsService } from './../contacts.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-create',
   templateUrl: './contact-create.component.html',
   styleUrls: ['./contact-create.component.css']
 })
-export class ContactCreateComponent implements OnInit {
+export class ContactCreateComponent{
 
-  constructor() { }
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),//inicializa com o campo vazio e é obrigatorio
+    phone: new FormControl('', Validators.required)
+  })
 
-  ngOnInit(): void {
+  constructor( private contactsService: ContactsService, private router: Router) { }
+
+  createContact(): void {
+    //para pegar o contato: this.contacForm.value
+    this.contactsService.createContact(this.contactForm.value).subscribe(contact=>{
+      //após adicionar o contato, retorna para a lista de contatos
+      this.router.navigate(["/contacts"]);
+    })
+
   }
-
 }
